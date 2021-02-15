4db7d35692a6032aaa6b3f541e285e1aa60426c5
silasplfilho <silasplfilho@outlook.com>
2021-02-15 16:18:54 -0300

    updt pytorch book

--- book_DeepLearningWithPytorch/cap5.py
+++ book_DeepLearningWithPytorch/cap5.py
@@ -1,1 +1,91 @@
-import pytorch
+import torch
+from matplotlib import pyplot as plt
+
+t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
+t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4] 
+t_c = torch.tensor(t_c) 
+t_u = torch.tensor(t_u)
+
+def model(t_u, w, b):
+    return w * t_u + b
+
+def loss_fn(t_p, t_c):
+    squared_diffs = (t_p - t_c)**2
+    return squared_diffs.mean()
+
+
+w = torch.ones(())
+
+b = torch.zeros(())
+
+t_p = model(t_u, w, b)
+
+loss = loss_fn(t_p, t_c)
+
+# ------
+delta = 0.1
+loss_rate_of_change_w = \
+    (loss_fn(model(t_u, w + delta, b), t_c) - 
+     loss_fn(model(t_u, w - delta, b), t_c)) / (2.0 * delta)
+
+learning_rate = 1e-2
+w = w -learning_rate * loss_rate_of_change_w
+
+loss_rate_of_change_b = \
+    (loss_fn(model(t_u, w, b+delta), t_c) -
+     loss_fn(model(t_u, w, b-delta), t_c)) / (2.0 * delta)
+
+b = b - learning_rate * loss_rate_of_change_b
+# 
+
+def dloss_fn(t_p, t_c): 
+    dsq_diffs = 2* (t_p - t_c) / t_p.size(0) 
+    return dsq_diffs
+
+
+def dmodel_dw(t_u, w, b): 
+    return t_u
+
+
+def dmodel_db(t_u, w, b): 
+    return 1.0
+
+
+def grad_fn(t_u, t_c, t_p, w, b):
+    dloss_dtp = dloss_fn(t_p, t_c)
+    dloss_dw = dloss_dtp * dmodel_dw(t_u, w, b) 
+    dloss_db = dloss_dtp * dmodel_db(t_u, w, b) 
+    return torch.stack([dloss_dw.sum(), dloss_db.sum()])
+
+
+def training_loop(n_epochs, learning_rate, params, t_u, t_c):
+    for epoch in range(1, n_epochs + 1):
+        w, b = params
+        
+        t_p = model(t_u, w, b)
+        loss = loss_fn(t_p, t_c) 
+        grad = grad_fn(t_u, t_c, t_p, w, b)
+        
+        params = params - learning_rate * grad 
+        
+        print('Epoch %d, Loss %f' % (epoch, float(loss)))
+    
+    return params
+
+t_un = 0.1 * t_u
+
+training_loop(n_epochs = 5000, 
+              learning_rate = 1e-2,
+              params = torch.tensor([1.0, 0.0]), 
+              t_u = t_un, 
+              t_c = t_c)
+#
+#  5.4.5
+t_p = model(t_un, *params)
+
+fig = plt.figure(dpi=20) 
+plt.xlabel("Temperature (°Fahrenheit)") 
+plt.ylabel("Temperature (°Celsius)") 
+plt.plot(t_u.numpy(), t_p.detach().numpy()) 
+plt.plot(t_u.numpy(), t_c.numpy(), 'o')
+plt.show()
---
+++ book_DeepLearningWithPytorch/cap5_2.py
@@ -0,0 +1,51 @@
+import torch
+import numpy as np
+from matplotlib import pyplot as plt
+
+t_c = torch.tensor([0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0])
+t_u = torch.tensor([35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4])
+t_un = 0.1 * t_u
+
+def model(t_u, w, b):
+    return w * t_u + b
+
+def loss_fn(t_p, t_c):
+    squared_diffs = (t_p - t_c)**2
+    return squared_diffs.mean()
+
+params = torch.tensor([1.0, 0.0], requires_grad=True)
+params.grad is None
+
+loss = loss_fn(model(t_u, *params), t_c)
+loss.backward()
+
+params.grad
+
+if params.grad is not None:
+    params.grad.zero_()
+
+
+def training_loop(n_epochs, learning_rate, params, t_u, t_c):
+    for epoch in range(1, n_epochs + 1):
+        if params.grad is not None:  # <1>
+            params.grad.zero_()
+
+        t_p = model(t_u, *params)
+        loss = loss_fn(t_p, t_c)
+        loss.backward()
+
+        with torch.no_grad():  # <2>
+            params -= learning_rate * params.grad
+
+        if epoch % 500 == 0:
+            print('Epoch %d, Loss %f' % (epoch, float(loss)))
+
+    return params
+
+
+training_loop(
+    n_epochs=5000,
+    learning_rate=1e-2,
+    params=torch.tensor([1.0, 0.0], requires_grad=True),  # <1>
+    t_u=t_un,  # <2>
+    t_c=t_c)
